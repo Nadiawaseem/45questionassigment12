f05809078c2f57815250fbf57bbde1768b3ea335
nadiawaseem <161966596+Nadiawaseem@users.noreply.github.com>
2024-06-08 16:40:42 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,6 @@
+"use strict";
+let names = ["Hammad", "Shehbaz", "naveed"];
+let message = 'Friendship is the golden thread that ties the heart of all the world:';
+for (let i = 0; i < names.length; i++) {
+    console.log(message, names[i]);
+}
---
+++ main.ts
@@ -0,0 +1,6 @@
+  let names :string [] = ["Hammad","Shehbaz", "naveed"];
+  let message :string =  'Friendship is the golden thread that ties the heart of all the world:'
+
+  for (let i = 0; i < names. length; i ++) {
+    console.log(message, names[i]);
+  }
